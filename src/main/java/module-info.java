c2714b8d8b783e52925ba50124dc98054988b447
Kennedy Kori <kennedykori47@gmail.com>
2024-01-05 11:43:32 +0300

    feat: modularize the project (#9)
    Define the module `io.github.kennedykori.utils` and migrate the project
    to use that module. The module exports the package
    `io.github.kennedykori.utils` and does not require any other modules.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * Defines useful utilities for working with primitives and objects.
+ *
+ * @since 2.0.0
+ */
+module io.github.kennedykori.utils {
+    exports io.github.kennedykori.utils;
+}
